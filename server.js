55eacbf2459bf4fdd32567c84b8a2c09503a5e91
sanketh <sanketh@gmail.com>
2025-02-24 13:36:07 +0530

    server.js-1

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-    res.send("Hello from ECS Fargate!");
+    res.send("Hello from ECS Fargate-green!");
 });
 
 app.listen(port, () => {
