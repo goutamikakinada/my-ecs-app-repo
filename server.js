9ca07dbc189c352e078a0ff583e395c4eb2c77a5
goutamikakinada <goutamikakinada1@gmail.com>
2025-04-23 14:32:07 +0530

    secound-commit

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-    res.send("Hello from ECS Fargate-blue!");
+    res.send("Hello FROM THE ACCUSHIELD");
 });
 
 app.listen(port, () => {
